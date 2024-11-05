a494d405e89dfcc702264dbb7fa8132014dd8be5
Maxim <madear0000@bk.ru>
2024-11-05 17:54:11 +0300

    add API key

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import telebot
-bot = telebot.TeleBot('')
+
+
+bot = telebot.TeleBot('7981308623:AAFQFaX8c-yOJZX-hYtG6LPlifqZQfXcTW0')
 
 @bot.message_handler(commands=['start'])
 def startChat(message):
-    bot.send_message(message.chat.id, 'Привет')
-    
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
     
 bot.polling(none_stop=True)
